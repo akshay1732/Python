855415fa41a73434ce297add3b80876d10ad4915
Akshay <Akshay@AKDesktop>
2019-11-12 01:41:07 +0530

    Python Commit

---
+++ src/StandardDataTypes.py
@@ -0,0 +1,60 @@
+'''
+Created on 11-Nov-2019
+
+@author: Akshay
+'''
+#Numbers
+''' 
+Python supports four different numerical types:
+
+int (signed integers)
+long (long integers, they can also be represented in octal and hexadecimal)
+float (floating point real values)
+complex (complex numbers)
+
+'''
+'''                          Integers and Float                         '''
+var1 = 1
+var2 = 10.5
+
+''' Hexadecimal Assignment'''
+var3 = -0x19323
+
+''' Octal Assignment'''
+var4 = 0o10
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+
+
+'''                         Complex Numbers                             '''
+
+c1=3.14j
+c2=3e+26J
+print(c1)
+print(c2)
+
+
+
+'''                         Strings                                    '''
+
+str1 = 'Hello World!'
+
+print ("\n",str1)         # Prints complete string
+print (str1[0])       # Prints first character of the string
+print (str1[2:5])     # Prints characters starting from 3rd to 5th
+print (str1[2:])      # Prints string starting from 3rd character
+print (str1 * 2)      # Prints string two times
+print (str1 + "TEST") # Prints concatenated string
+
+
+
+
+'''delete variables'''
+
+del var1,var2
+
+''' '''
---
+++ src/Variables.py
@@ -0,0 +1,21 @@
+'''
+Created on 11-Nov-2019
+
+@author: Akshay
+'''
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print (counter)
+print (miles)
+print (name)
+
+#Multiple Assignment
+a = b = c = 1
+a,b,c = 1,2,"John"
+
+
+print("\n",b)
+print(c)
